d0346ac876aacbb5eb9cae6a2b72d2d872b5ab8b
Amol Jagtap <amoljagtap17@gmail.com>
2019-04-20 17:23:39 +0530

    GATSBY_BOOTCAMP: Add gatsby config and node settings

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,60 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+// Not needed with Contentful
+
+/* module.exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === 'MarkdownRemark') {
+    // console.log(JSON.stringify(node, undefined, 4))
+    const slug = path.basename(node.fileAbsolutePath, '.md')
+    // console.log('--------', slug)
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug
+    })
+  }
+} */
+
+module.exports.createPages = async ({ graphql, actions}) => {
+  const { createPage } = actions
+  const blogTemplate = path.resolve('./src/templates/blog.js')
+
+  /* const response = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }     
+  `) */
+
+  const response = await graphql(`
+    {
+      allContentfulBlogPost {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }     
+  `)
+
+  response.data.allContentfulBlogPost.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.slug}`,
+      context: {
+        slug: edge.node.slug
+      }
+    })
+  })
+}
