a61f5f90360c0e75852ccaa4e163a2fa1460aada
Amol Jagtap <amoljagtap17@gmail.com>
2019-04-20 17:37:22 +0530

    GATSBY_BOOTCAMP: Make change to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const ContactPage = () => {
     <Layout>
       <Head title="Contact" />
       <h1>Contact</h1>
-      <p>The best way to reach me is via <a href="https://www.github.com" target="_blank" rel="noopener noreferrer">github/xyz</a></p>
+      {/* <p>The best way to reach me is via <a href="https://www.github.com" target="_blank" rel="noopener noreferrer">github/xyz</a></p> */}
     </Layout>
   )
 }
